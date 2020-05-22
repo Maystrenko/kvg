93fd37179722d6171cf180952a2dce3a4be8bbe9
Ihor Maystrenko <maystrenk@gmail.com>
2020-05-22 13:17:51 +0300

    набор скриптов

---
+++ kyivgaz/test.php
@@ -0,0 +1,1 @@
+<iframe id="ifr" src="https://docs.google.com/forms/d/e/1FAIpQLSdd0IjAD91ubZv2sPb5L2VUxJAGcFdPhRxzx-mqXlqsUhrOzg/viewform" width="100%" height="1300px"  frameborder="0" scrolling="off"></iframe>
